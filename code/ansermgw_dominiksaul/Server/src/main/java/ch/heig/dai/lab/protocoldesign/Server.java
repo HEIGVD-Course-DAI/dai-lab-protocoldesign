e35a3859a0692328dad0cf81f7b4db685a8f62e0
Dominik Saul <dominik@saul.ch>
2023-10-19 14:58:17 +0200

    start implementation of server

--- code/ansermgw_dominiksaul/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/ansermgw_dominiksaul/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,7 +1,13 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Server {
-    final int SERVER_PORT = 1234;
+    final int SERVER_PORT = 4242;
 
     public static void main(String[] args) {
         // Create a new server and run it
@@ -10,5 +16,23 @@ public class Server {
     }
 
     private void run() {
-    } 
+        // Create a passive socket (class ServerSocket)
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     var in = new BufferedReader(
+                             new InputStreamReader(
+                                     socket.getInputStream(), UTF_8));
+                     var out = new BufferedWriter(
+                             new OutputStreamWriter(
+                                     socket.getOutputStream(), UTF_8))) {
+
+                } catch (IOException e) {
+                    System.out.println("Socket Exception: " + e);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("ServerSocket Exception: " + e);
+        }
+    }
 }
