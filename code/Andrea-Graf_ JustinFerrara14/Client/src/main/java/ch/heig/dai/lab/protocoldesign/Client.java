b3b7f0649971d141c47603dc2f6fafbf7a57cea2
Andrea Graf <andrea.graf@heig-vd.ch>
2023-10-19 11:52:42 +0200

    First test

--- code/Andrea-Graf_ JustinFerrara14/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/Andrea-Graf_ JustinFerrara14/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,9 +1,9 @@
 package ch.heig.dai.lab.protocoldesign;
 
+
 public class Client {
     final String SERVER_ADDRESS = "1.2.3.4";
     final int SERVER_PORT = 1234;
-
     public static void main(String[] args) {
         // Create a new client and run it
         Client client = new Client();
@@ -11,5 +11,26 @@ public class Client {
     }
 
     private void run() {
+        system.out.println("Client running");
+
+        try (Socket socket = new Socket("localhost", SERVER_PORT));
+        var in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream(),
+                        StandardCharsets.UTF_8));
+        var out = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream(),
+                        StandardCharsets.UTF_8))){
+            system.out.println("Connexion ok");
+            system.out.println("Entree votre calcul : ");
+            Scanner scanner = new Scanner(System.in);
+            String ligne = scanner.nextLine();
+            system.out.println(in.read(););
+            out.write(calcul);
+            out.flush();
+            }
+        } catch (IOException e) {
+            System.out.println("Client: exc.: " + e);
+        }
+
     }
 }
