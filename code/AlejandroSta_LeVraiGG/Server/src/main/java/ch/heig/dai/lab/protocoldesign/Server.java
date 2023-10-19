6dd9a9d0df1aee1ab56ad838d9e6a13d8de99e69
AlejandroSta <alejandro.stadlin@heig-vd.ch>
2023-10-19 11:59:11 +0200

    imports server part 1

--- code/AlejandroSta_LeVraiGG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/AlejandroSta_LeVraiGG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,7 +1,16 @@
 package ch.heig.dai.lab.protocoldesign;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.*;
 
 public class Server {
     final int SERVER_PORT = 4242;
+    final String[] OPERATIONS = {"ADD", "MULL", "SUB", "DIV"};
 
     public static void main(String[] args) {
         // Create a new server and run it
@@ -14,14 +23,11 @@ public class Server {
         try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
             while (true) {
                 try (Socket socket = serverSocket.accept();
-                     InputStream in = new BufferedInputStream(socket.getInputStream());
+                     InputStream in = new BufferedInputStream(socket.getInputStream(), Charset.UTF_8);
                      OutputStream out = new
                              BufferedOutputStream(socket.getOutputStream());) {
-                    int i;
-                    while ((i = in.read()) != -1) {
-                        out.write(i);
-                        out.flush();
-                    }
+                    //TODO: lecture du paquet et traitement
+                    String op = in.
                 } catch (IOException e) {
                     System.out.println("Server: socket ex. : " + e);
                 }
