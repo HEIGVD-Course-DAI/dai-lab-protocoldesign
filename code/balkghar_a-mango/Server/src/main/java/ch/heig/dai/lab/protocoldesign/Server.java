d2d0834de5093076d29612d253dae92e41ef8101
Balkghar <hugo.germano8@gmail.com>
2023-10-21 19:00:05 +0200

    wip: end of basic function, need some tuning

--- code/balkghar_a-mango/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/balkghar_a-mango/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -55,29 +55,44 @@ public class Server {
 			System.out.print(token + "\t");
 			switch (token) {
 				case "+":
-					System.out.print("Operate\t\t");
 					stack.push(stack.pop() + stack.pop());
 					break;
 				case "-":
-					System.out.print("Operate\t\t");
 					stack.push(-stack.pop() + stack.pop());
 					break;
 				case "*":
-					System.out.print("Operate\t\t");
 					stack.push(stack.pop() * stack.pop());
 					break;
 				case "/":
-					System.out.print("Operate\t\t");
 					double divisor = stack.pop();
 					stack.push(stack.pop() / divisor);
 					break;
 				case "^":
-					System.out.print("Operate\t\t");
 					double exponent = stack.pop();
 					stack.push(Math.pow(stack.pop(), exponent));
 					break;
+				case "π":
+					stack.push(Math.PI);
+					break;
+				case "√":
+					stack.push(Math.sqrt(stack.pop()));
+					break;
+				case "e":
+					stack.push(Math.E);
+					break;
+				case "sin":
+					stack.push(Math.sin(stack.pop()));
+					break;
+				case "cos":
+					stack.push(Math.cos(stack.pop()));
+					break;
+				case "tan":
+					stack.push(Math.tan(stack.pop()));
+					break;
+				case "ln":
+					stack.push(Math.log(stack.pop()));
+					break;
 				default:
-					System.out.print("Push\t\t");
 					stack.push(Double.parseDouble(token));
 					break;
 			}
