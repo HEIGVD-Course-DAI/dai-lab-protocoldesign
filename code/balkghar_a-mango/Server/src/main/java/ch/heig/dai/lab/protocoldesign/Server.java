24980a55772c9a3c4fe4fdc989e3c30b0b8231f1
Balkghar <hugo.germano8@gmail.com>
2023-10-21 20:44:47 +0200

    wip: only small part to be done to be ready to PR

--- code/balkghar_a-mango/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/balkghar_a-mango/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -8,11 +8,50 @@ import java.util.Stack;
 
 public class Server {
 	final int SERVER_PORT = 51740;
+	final String BASE_MESSAGE = "GCCP";
+	final String INFO_MESSAGE = "HELLO";
+	final String END_MESSAGE = "BYE";
+	final String EASTER_EGG_CODE = "🟢🐱";
+	final String EASTER_EGG_TEXT = "                             .                                   \n" + //
+			"                          ,''`.         _                        \n" + //
+			"                     ,.,'''  '`--- ._,,'|                        \n" + //
+			"                   ,'                   /                        \n" + //
+			"              __.-'                    |                         \n" + //
+			"           ''                ___   ___ |                         \n" + //
+			"         ,'                  \\(|\\ /|)/ |                         \n" + //
+			"        ,'                 _     _     `._                       \n" + //
+			"       /     ,.......-\\    `.      __     `-.                    \n" + //
+			"      /     ,' :  .:''`|    `:`.../:.`` ._   `._                 \n" + //
+			"  ,..,'  _/' .: :'     |     |      '.    \\.    \\                \n" + //
+			" /      ,'  :'.:  / \\  |     | / \\   ':.  . \\    \\               \n" + //
+			"|      /  .: :' ,'  _)  \".._,;'  _)    :. :. \\    |              \n" + //
+			" |     | :'.:  /   |   .,   /   |       :  :  |   |              \n" + //
+			" |     |:' :  /____|  /  \\ /____|       :  :  |  ,'              \n" + //
+			"  |   /    '         /    \\            :'   : |,/                \n" + //
+			"   \\ |  '_          /______\\              , : |                  \n" + //
+			"  _/ |  \\'`--`.    _            ,_   ,-'''  :.|         __       \n" + //
+			" /   |   \\..   ` ./ `.   _,_  ,'  ``'  /'   :'|      _,''/       \n" + //
+			"/   /'. :   \\.   _    [_]   `[_]  .__,,|   _....,--=/'  /:       \n" + //
+			"|   \\_| :    `.-' `.    _.._     /     . ,'  :. ':/'  /'  `.     \n" + //
+			"`.   '`'`.         `. ,.'   ` .,'     :'/ ':..':.    |  .:' `.   \n" + //
+			"  \\.      \\          '               :' |    ''''      ''     `. \n" + //
+			"    `''.   `|        ':     .      .:' ,|         .  ..':.      |\n" + //
+			"      /'   / '\"-..._  :   .:'    _;:.,'  \\.     .:'   :. ''.    |\n" + //
+			"     (._,.'        '`''''''''''''          \\.._.:      ':  ':   /\n" + //
+			"________                                      '`- ._    ,:__,,-' \n" + //
+			"  |                                                 ``''\n";
 
 	public static void main(String[] args) {
 		// Create a new server and run it
 		Server server = new Server();
 		server.run();
+	}
+
+	static double factorial(double n) {
+		if (n == 0)
+			return 1;
+		else
+			return (n * factorial(n - 1));
 	}
 
 	private void run() {
@@ -23,20 +62,34 @@ public class Server {
 						BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
 						BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8))) {
 
-					String line = in.readLine();
+					String line;
 
-					if (line != null && line.substring(0, 4) == "CALC") {
-						double result = calculation(line.substring(5));
-						out.write("CALC " + String.valueOf(result));
-					} else {
-						out.write("CALC ERR 1");
+					while ((line = in.readLine()) != null) {
+						if (line.substring(0, 4).equals(BASE_MESSAGE)) {
+							if (line.substring(5, 8).equals(END_MESSAGE)) {
+								out.write(BASE_MESSAGE + " " + END_MESSAGE);
+								out.flush();
+								in.close();
+								out.close();
+							} else if (line.substring(5, 9).equals(EASTER_EGG_CODE)) {
+								out.write(EASTER_EGG_TEXT);
+							} else if (line.substring(5, 10).equals(INFO_MESSAGE)) {
+								out.write("GCCP HELLO <OPERATIONS>\n" + " Example : GCCP HELLO + * / sin ln\n");
+							} else {
+								out.write(BASE_MESSAGE + " " + String.valueOf(calculation(line.substring(5))) + "\n");
+							}
+							out.flush();
+						}
 					}
+
 				} catch (IOException e) {
 					System.out.println("Server: socket ex.: " + e);
+					return;
 				}
 			}
 		} catch (IOException e) {
 			System.out.println("Server: server socket ex.: " + e);
+			return;
 		}
 	}
 
@@ -83,6 +136,9 @@ public class Server {
 					break;
 				case "ln":
 					stack.push(Math.log(stack.pop()));
+					break;
+				case "!":
+					stack.push(factorial(stack.pop()));
 					break;
 				default:
 					try {
