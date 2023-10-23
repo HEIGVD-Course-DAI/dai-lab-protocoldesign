b4c3100ea6170f5a99be852aabff5c7ebbefafca
G-LK <114987519+G-LK@users.noreply.github.com>
2023-10-23 16:31:26 +0200

    update on server file

--- code/samuelroland_G-LK/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/samuelroland_G-LK/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,5 +1,8 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.*;
+
 public class Server {
     final int SERVER_PORT = 1234;
 
@@ -10,5 +13,36 @@ public class Server {
     }
 
     private void run() {
-    } 
+
+
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT);) {
+
+            while (true) {
+                //create a server to be able to hold client connections
+
+                System.out.println("Server sockt is up");
+
+                try (Socket clientSocket = serverSocket.accept();) {
+                    //wait for a client to be connected with the server
+
+                    System.out.println("Server is waiting for clients...");
+
+                } catch (IOException e) {
+                    System.out.println("Server: server socket ex.: " + e);
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Server: server socket ex.: " + e);
+        }
+
+
+        //establish the connection with the client
+        //Socket socket = new Socket(clientSocket, SERVER_PORT);
+        //System.out.println("Connection established with ");
+
+
+    }
+
+
 }
