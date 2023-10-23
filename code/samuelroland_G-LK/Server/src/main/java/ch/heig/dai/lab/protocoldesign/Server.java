9d8367eae96fdbe746ea6ad86b58fd933e241e9c
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-10-23 16:48:14 +0200

    Basic duplicated response from server

--- code/samuelroland_G-LK/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/samuelroland_G-LK/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -2,47 +2,45 @@ package ch.heig.dai.lab.protocoldesign;
 
 import java.io.*;
 import java.net.*;
+import static java.nio.charset.StandardCharsets.*;
 
 public class Server {
-    final int SERVER_PORT = 1234;
+	final int SERVER_PORT = 1234;
 
-    public static void main(String[] args) {
-        // Create a new server and run it
-        Server server = new Server();
-        server.run();
-    }
+	public static void main(String[] args) {
+		// Create a new server and run it
+		Server server = new Server();
+		server.run();
+	}
 
-    private void run() {
+	private void run() {
+		try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT);) {
+			while (true) {
+				// Create a passive socket to wait for new connections
+				System.out.println("Server socket is up");
 
+				// Create an active socket once we get a client
+				try (Socket socket = serverSocket.accept();
+						var in = new BufferedReader(
+								new InputStreamReader(
+										socket.getInputStream(), UTF_8));
+						var out = new BufferedWriter(
+								new OutputStreamWriter(
+										socket.getOutputStream(), UTF_8))) {
 
-        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT);) {
-
-            while (true) {
-                //create a server to be able to hold client connections
-
-                System.out.println("Server sockt is up");
-
-                try (Socket clientSocket = serverSocket.accept();) {
-                    //wait for a client to be connected with the server
-
-                    System.out.println("Server is waiting for clients...");
-
-                } catch (IOException e) {
-                    System.out.println("Server: server socket ex.: " + e);
-                }
-            }
-
-        } catch (IOException e) {
-            System.out.println("Server: server socket ex.: " + e);
-        }
-
-
-        //establish the connection with the client
-        //Socket socket = new Socket(clientSocket, SERVER_PORT);
-        //System.out.println("Connection established with ");
-
-
-    }
-
+					// wait for a client to be connected with the server
+					String line;
+					while ((line = in.readLine()) != null) {
+						out.write(line + "\n");
+						out.flush();
+					}
+				} catch (IOException e) {
+					System.out.println("Server: server socket ex.: " + e);
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Server: server socket ex.: " + e);
+		}
+	}
 
 }
