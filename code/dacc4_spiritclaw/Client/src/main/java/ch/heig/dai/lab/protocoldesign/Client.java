e2cd3702ba94c66e20468032347104ff937c4749
Christophe - CC4 <roulin43@gmail.com>
2023-10-23 17:35:34 +0200

    wip

--- code/dacc4_spiritclaw/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/dacc4_spiritclaw/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,8 +1,15 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
-    final int SERVER_PORT = 1234;
+    private final String SERVER_ADDRESS = "1.2.3.4";
+    private final int SERVER_PORT = 4242;
+    private final Charset charset = StandardCharsets.UTF_8;
+    private final char lineSeparator = ';';
 
     public static void main(String[] args) {
         // Create a new client and run it
@@ -11,5 +18,22 @@ public class Client {
     }
 
     private void run() {
+    }
+
+    private ArrayList<Operation> convertFromString(String str){
+        ArrayList<Operation> operations = new ArrayList<>();
+
+        // TODO : Logic
+
+        return operations;
+    }
+
+    private OperationResult computeOperations(ArrayList<Operation> operations){
+        try (var socket = new Socket(SERVER_ADDRESS, SERVER_PORT)){
+
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
     }
 }
