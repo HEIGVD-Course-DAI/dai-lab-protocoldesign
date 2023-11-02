468bbed329d1ce95f5f5bfca472d5f731446002c
AlejandroSta <alejandro.stadlin@heig-vd.ch>
2023-11-02 11:20:47 +0100

    server v0.1

--- code/AlejandroSta_LeVraiGG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/AlejandroSta_LeVraiGG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -2,12 +2,13 @@ package ch.heig.dai.lab.protocoldesign;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
 
 import static java.nio.charset.StandardCharsets.*;
 
 public class Server {
     final int SERVER_PORT = 4242;
-    final String[] OPERATIONS = {"ADD", "MULL", "SUB", "DIV"};
+    final String[] OPERATIONS = {"ADD", "MUL"/*, "SUB", "DIV"*/};
 
     public static void main(String[] args) {
         // Create a new server and run it
@@ -25,7 +26,42 @@ public class Server {
                      BufferedWriter out = new BufferedWriter(
                                           new OutputStreamWriter(socket.getOutputStream(), UTF_8))) {
                     //TODO : lecture du paquet et traitement
-                    String op;
+                    String line;
+                    while ((line = in.readLine()) != null){
+                        String[] args = line.split(" ");
+                        double value1, value2, result = 0;
+                        if(args.length != 3){
+                            out.write("INVALID 2" + '\n');
+                            out.flush();
+                            socket.close();
+                        }
+                        if(!Arrays.asList(OPERATIONS).contains(args[0])){
+                            out.write("INVALID 1" + '\n');
+                            out.flush();
+                            socket.close();
+                        }
+                        if(!args[1].startsWith("+-") || !args[2].startsWith("+-")){
+                            out.write("INVALID 3" + '\n');
+                            out.flush();
+                            socket.close();
+                        }
+                        try{
+                            value1 = Double.parseDouble(args[1]);
+                            value2 = Double.parseDouble(args[2]);
+                            result = switch (args[0]) {
+                                case "ADD" -> value1 + value2;
+                                case "MUL" -> value1 * value2;
+                                default -> result;
+                            };
+                            if(result >= 0) out.write("+");
+                            out.write(Double.toString(result));
+
+                        }catch (NumberFormatException e){
+                            out.write("INVALID 3" + '\n');
+                            out.flush();
+                            socket.close();
+                        }
+                    }
                 } catch (IOException e) {
                     System.out.println("Server: socket ex. : " + e);
                 }
