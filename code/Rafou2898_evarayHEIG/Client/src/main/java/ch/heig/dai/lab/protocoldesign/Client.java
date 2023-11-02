202ea2c870ffd17dae1eddcb9c699dd12133e9b6
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2023-11-02 15:31:07 +0100

    feat: implementation of client and server

--- code/Rafou2898_evarayHEIG/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/Rafou2898_evarayHEIG/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,8 +1,16 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
-    final int SERVER_PORT = 1234;
+    final String SERVER_ADDRESS = "localhost";
+    final int SERVER_PORT = 2828;
+    final String WELCOME_MSG = "WELCOME";
+    final String ERROR_UNKOWN = "UNKOWN";
+    final String ERROR_INVALID = "INVALID";
 
     public static void main(String[] args) {
         // Create a new client and run it
@@ -11,5 +19,39 @@ public class Client {
     }
 
     private void run() {
+
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+             var scanner = new Scanner(System.in)) {
+
+            String serverMessage = in.readLine();
+            String userInput;
+            String result;
+
+            if (serverMessage.contains(WELCOME_MSG)) {
+                System.out.println("Enter your calculation: ");
+                userInput = scanner.nextLine();
+
+                out.write(userInput + "\n");
+                out.flush();
+
+                result = in.readLine();
+
+                if(result.equals(ERROR_INVALID)){
+                    throw new IOException("Client: One of the operand isn't a number");
+                }
+
+                if(result.equals(ERROR_UNKOWN)){
+                    throw new IOException("Client: The operation you asked for does not exist.");
+                }
+
+                System.out.println("Here is the result of the calculation: " + result);
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Client: exception while using client socket: " + e);
+        }
     }
 }
--- code/Rafou2898_evarayHEIG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/Rafou2898_evarayHEIG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,7 +1,21 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.*;
+
+import static java.nio.charset.StandardCharsets.*;
+
 public class Server {
-    final int SERVER_PORT = 1234;
+    final int SERVER_PORT = 2828;
+    final String WELCOME_MSG = "WELCOME: You can do the following operations: ADD, MULT";
+    final String ERROR_UNKOWN = "UNKOWN";
+    final String ERROR_INVALID = "INVALID";
+
+    enum Operations
+    {
+        ADD,
+        MULT
+    }
 
     public static void main(String[] args) {
         // Create a new server and run it
@@ -10,5 +24,51 @@ public class Server {
     }
 
     private void run() {
-    } 
+
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+
+            while (true) {
+
+                try (Socket socket = serverSocket.accept();
+                     var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
+                     var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), UTF_8))) {
+
+                    out.write(WELCOME_MSG + "\n");
+                    out.flush();
+
+                    String userInput = in.readLine();
+                    // Operation is at index 0, operand 1 at index 1, operand 2 at index 2
+                    String[] parameters = userInput.split(" ");
+
+                    try{
+                        Double.parseDouble(parameters[1]);
+                        Double.parseDouble(parameters[2]);
+                    } catch (NumberFormatException e){
+                        System.out.println("Server: socket ex.: " + e);
+                        out.write(ERROR_INVALID);
+                        socket.close();
+                    }
+
+                    switch (parameters[0]) {
+                        case "ADD":
+                            out.write(String.valueOf(Double.parseDouble(parameters[1]) + Double.parseDouble(parameters[2])) + "\n");
+                            break;
+                        case "MULT":
+                            out.write(String.valueOf(Double.parseDouble(parameters[1]) * Double.parseDouble(parameters[2])) + "\n");
+                            break;
+                        default:
+                            out.write(ERROR_UNKOWN + "\n");
+                            break;
+                    }
+
+                    socket.close();
+
+                } catch (IOException e) {
+                    System.out.println("Server: socket ex.: " + e);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Server: server socket ex.: " + e);
+        }
+    }
 }
