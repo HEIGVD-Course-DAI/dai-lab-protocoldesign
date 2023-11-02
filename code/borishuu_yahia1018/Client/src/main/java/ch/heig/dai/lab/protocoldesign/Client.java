6e3bbd4c033ec6a992d067c528fa3a67308bcebd
yahia1018 <yanis.ouadahi@gmail.com>
2023-11-02 11:53:40 +0100

    Client input sending implementation

--- code/borishuu_yahia1018/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/borishuu_yahia1018/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -3,6 +3,7 @@ import javax.xml.stream.events.StartDocument;
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Client {
     final String SERVER_ADDRESS = "1.2.3.4";
@@ -19,21 +20,26 @@ public class Client {
         System.out.print("Supported operations: " + SUPPORTED_OPERATIONS[0]);
         for (int i = 1; i < SUPPORTED_OPERATIONS.length; ++i)
             System.out.print(", " + SUPPORTED_OPERATIONS[i]);
+        System.out.print("\n");
     }
 
     private void run() {
         welcomeMessage();
 
         try (Socket socket = new Socket("localhost", SERVER_PORT)) {
+            Scanner userInput = new Scanner(System.in);
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             BufferedWriter out = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
 
-            for (int i = 0; i < 10; ++i) {
-                out.write("Hello " + i + "\n");
+            while (true) {
+                System.out.println("Entrez une operation que vous souhaitez effectuer: ");
+
+                out.write(userInput.nextLine() + '\n');
                 out.flush();
-                System.out.println("Echo: " + in.readLine());
+
+                System.out.println("Server response: " + in.readLine());
             }
         } catch (IOException e) {
             System.out.println("Client: exception: " + e);
