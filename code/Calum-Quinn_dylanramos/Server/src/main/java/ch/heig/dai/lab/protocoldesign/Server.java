7d6dd6ffefb9a146af53348b9941b5b41798d9f7
calum <calum@quinns.ch>
2023-11-02 14:11:30 +0100

    Complete server implementation

--- code/Calum-Quinn_dylanramos/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/Calum-Quinn_dylanramos/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -8,12 +8,12 @@ import java.util.Objects;
 
 public class Server {
     final int SERVER_PORT = 32976;
-    final String OPERATIONS =   "ADD <firstNumber> <secondNumber>\n" +
-                                "SUB <firstNumber> <secondNumber>\n" +
-                                "MUL <firstNumber> <secondNumber>\n" +
-                                "DIV <firstNumber> <secondNumber>\n" +
-                                "END\n";
-    static final String[] OPS = {"ADD","SUB","MUL","DIV"};
+    final String OPERATIONS = "ADD <firstNumber> <secondNumber>\n" +
+            "SUB <firstNumber> <secondNumber>\n" +
+            "MUL <firstNumber> <secondNumber>\n" +
+            "DIV <firstNumber> <secondNumber>\n" +
+            "END\n";
+    static final String[] OPS = {"ADD", "SUB", "MUL", "DIV"};
 
     public static void main(String[] args) {
         // Create a new server and run it
@@ -23,7 +23,7 @@ public class Server {
 
     // Check if the number string represents a number
     public static boolean isNumeric(String number) {
-        return number.matches("-?\\d+");
+        return number.matches("[+-]?\\d+");
     }
 
     // Carries out the operation
@@ -31,14 +31,18 @@ public class Server {
         int number1 = Integer.parseInt(num1);
         int number2 = Integer.parseInt(num2);
         switch (op) {
-            case "ADD" : return Integer.toString(number1 + number2);
-            case "SUB" : return Integer.toString(number1 - number2);
-            case "MUL" : return Integer.toString(number1 * number2);
-            case "DIV" :
+            case "ADD":
+                return Integer.toString(number1 + number2) + '\n';
+            case "SUB":
+                return Integer.toString(number1 - number2) + '\n';
+            case "MUL":
+                return Integer.toString(number1 * number2) + '\n';
+            case "DIV":
                 if (number2 != 0) {
-                    return Integer.toString(number1 / number2);
+                    return Integer.toString(number1 / number2) + '\n';
                 }
-            default: return "UNSUPPORTED OPP";
+            default:
+                return "UNSUPPORTED OPP\n";
         }
     }
 
@@ -48,46 +52,35 @@ public class Server {
                  var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
                  var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
 
+                // Send possible operations to client
                 out.write(OPERATIONS);
                 out.flush();
 
+                // Read client's request
+                String request;
+                while ((request = in.readLine()) != null) {
+                    String[] arguments = request.split("\\s+");
+                    String opp = arguments[0];
+                    String num1 = arguments[1];
+                    String num2 = arguments[2];
+                    boolean validOp = false;
 
-                String[] request = in.readLine().split(" ");
-                String opp = request[0];
-                String num1 = request[1];
-                String num2 = request[2];
-                boolean validOp = false;
-
-                for (String op : OPS) {
-                    if (Objects.equals(opp, op)) {
-                        validOp = true;
-                        break;
-                    }
-                }
-
-                // Check if valid operation
-                if (!validOp) {
-                    out.write("UNSUPPORTED OPP");
-                    out.flush();
-                }
-                else {
-                    if(!(isNumeric(num1) && isNumeric(num2))) {
-                        out.write("UNSUPPORTED NUM");
+                    // Check if numbers are valid
+                    if (!(isNumeric(num1) && isNumeric(num2))) {
+                        out.write("UNSUPPORTED NUM\n");
                         out.flush();
-                    }
-                    else {
-                        // Valid operation and numbers
-                        out.write(operation(opp,num1,num2));
+                    } else {
+                        // Valid numbers
+                        out.write(operation(opp, num1, num2));
                         out.flush();
                     }
                 }
-            }
-            catch(IOException e) {
+
+            } catch (IOException e) {
                 System.out.println("Server: socket ex.: " + e);
             }
 
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println(("Server: server socket ex.: " + e));
         }
     }
