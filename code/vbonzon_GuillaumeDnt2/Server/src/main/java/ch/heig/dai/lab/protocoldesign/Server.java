b071ed5a43a42ee51a3e458c1e74a50ab98b90ab
vbonzon <113915093+vbonzon@users.noreply.github.com>
2023-11-02 11:09:17 +0100

    Update double + div 0

--- code/vbonzon_GuillaumeDnt2/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/vbonzon_GuillaumeDnt2/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -2,18 +2,22 @@ package ch.heig.dai.lab.protocoldesign;
 import java.net.*;
 import java.io.*;
 import java.lang.Math.*;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static java.nio.charset.StandardCharsets.*;
 
 public class Server {
-    final int SERVER_PORT = 3141;
+    final int SERVER_PORT = 31415;
     enum errors {BAD_ARG, BAD_VAL, BAD_OP};
     String[] OP = {"ADD", "MULT", "SUB", "DIV", "POW", "MEAN", "CLOSE"};
 
     boolean errorHappened = false;
 
     private BufferedWriter output;
-    //final HashMap<OP, String> operations = new HashMap<OP, String>();
+    
 
     public static void main(String[] args) {
         // Create a new server and run it
@@ -33,11 +37,11 @@ public class Server {
 
                         System.out.println("Connection enabled");
                         String welcomeMessage = "Voici les operations : ";
-                        output = out;
+                        
                         for(String s : OP){
                             welcomeMessage += (s + " ");
                         }
-                        
+                        output = out;
                         out.write(welcomeMessage + "\n");
                         out.flush();
                         String line;
@@ -56,6 +60,7 @@ public class Server {
                             }
                             else{
                                 
+                                //Get the result of the opperation and send it if no error happened
                                 String resultLine = operationSelector(split);
                                 if(!errorHappened){
                                     out.write(resultLine + "\n");
@@ -70,12 +75,10 @@ public class Server {
 
                     
                 } catch (Exception e) {
-                    // TODO: handle exception
                     System.out.println(e);
                 }
             }
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println(e);
         }
 
@@ -87,26 +90,26 @@ public class Server {
             errorMessage(errors.BAD_ARG);
         
         String output = "";
-        Integer result = 0;
+        Double result = 0.0;
         switch (input[0]){
 
             case "ADD":
-                result = add(stringToInt(input));
+                result = add(stringToDouble(input));
                 break;
             case "POW":
-                result = pow(stringToInt(input));
+                result = pow(stringToDouble(input));
                 break;    
             case "MULT":
-                result = mult(stringToInt(input));
+                result = mult(stringToDouble(input));
                 break; 
             case "DIV":
-                result = div(stringToInt(input));
+                result = div(stringToDouble(input));
                 break; 
             case "SUB":
-                result = sub(stringToInt(input));
+                result = sub(stringToDouble(input));
                 break;    
             case "MEAN":
-                result = mean(stringToInt(input));
+                result = mean(stringToDouble(input));
                 break;  
                      
             default:
@@ -136,6 +139,7 @@ public class Server {
                 break;
             case BAD_VAL: 
                 errorMsg += "Argument wrong type";
+                break;
             
         }
 
@@ -165,6 +169,22 @@ public class Server {
         return value;
     }
 
+    private double[] stringToDouble(String[] input){
+        double[] value = new double[input.length -1];
+        for(int i = 0; i < input.length-1; i++){
+            try {
+                value[i] = Double.parseDouble(input[i+1]);
+            } catch (Exception e) {
+                // TODO: handle exception
+                errorMessage(errors.BAD_VAL);
+            }
+            
+        }
+
+
+        return value;
+    }
+
     private int add(int[] values){
         
         int sum = 0;
@@ -175,9 +195,27 @@ public class Server {
         return sum;
     }
 
+    public double add(double[] values) {   
+        double sum = 0.0;
+        for(double v : values){
+            sum += v;
+        }
+        return sum;
+    }
+
+    
+
     private int mult(int[] values){
         int mult = 1;
         for(int v : values){
+            mult *= v;
+        }
+        return mult;
+    }
+
+    private double mult(double[] values){
+        double mult = 1.0;
+        for(double v : values){
             mult *= v;
         }
         return mult;
@@ -189,7 +227,15 @@ public class Server {
         for(int v  = 1; v < values.length; v++){
             diff -= values[v];
         }
+        return diff;
+    }
+
+    private double sub(double[] values){ //TODO
         
+        double diff = values[0];
+        for(int v  = 1; v < values.length; v++){
+            diff -= values[v];
+        }
         return diff;
     }
 
@@ -197,6 +243,20 @@ public class Server {
         
         int quot = values[0];
         for(int v  = 1; v < values.length; v++){
+            if(values[v] == 0)
+                errorMessage(errors.BAD_VAL);
+            quot /= values[v];
+        }
+        
+        return quot;
+    }
+
+    private double div(double[] values){ //TODO
+        
+        double quot = values[0];
+        for(int v  = 1; v < values.length; v++){
+            if(values[v] == 0 || values[v] == 0.0)
+                errorMessage(errors.BAD_VAL);
             quot /= values[v];
         }
         
@@ -212,8 +272,26 @@ public class Server {
         
     }
 
+    private double pow(double[] values){
+        
+        if(values.length > 2){
+            errorMessage(errors.BAD_ARG);
+        }
+        if(values[1] % 1 != 0){
+            errorMessage(errors.BAD_VAL);
+        }
+            
+        return (double)(Math.pow(values[0], values[1]));
+        
+    }
+
     private int mean(int[] values){
         int sum = add(values);
+        return sum/values.length;
+    }
+
+    private double mean(double[] values){
+        double sum = add(values);
         return sum/values.length;
     }
 
