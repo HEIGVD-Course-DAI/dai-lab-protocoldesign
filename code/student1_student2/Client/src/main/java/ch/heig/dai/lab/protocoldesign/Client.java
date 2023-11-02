efef81daf337843feab03ca2be14aceb779fb734
mehdibenz0 <146178244+mehdibenz0@users.noreply.github.com>
2023-11-02 11:03:50 +0100

    test push

--- code/student1_student2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/student1_student2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -6,6 +6,7 @@ public class Client {
 
     public static void main(String[] args) {
         // Create a new client and run it
+        //push test
         Client client = new Client();
         client.run();
     }
