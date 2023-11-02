6e09be39680b61f156cc3d5e9ce8cde4f7fc8403
mehdibenz0 <146178244+mehdibenz0@users.noreply.github.com>
2023-11-02 15:21:13 +0100

    final

--- code/student1_student2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/student1_student2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,16 +1,39 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
     final String SERVER_ADDRESS = "1.2.3.4";
     final int SERVER_PORT = 1234;
 
     public static void main(String[] args) {
         // Create a new client and run it
-        //push test
         Client client = new Client();
         client.run();
     }
 
     private void run() {
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Connected to the server.");
+
+            while (true) {
+                System.out.print("Enter a command (e.g., ADD 10 20): ");
+                String userInputStr = userInput.readLine();
+                out.println(userInputStr);
+
+                String response = in.readLine();
+                System.out.println("Server response: " + response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- code/student1_student2/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/student1_student2/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,5 +1,12 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     final int SERVER_PORT = 1234;
 
@@ -10,5 +17,54 @@ public class Server {
     }
 
     private void run() {
-    } 
+        try {
+            try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+                System.out.println("Server is listening on port " + SERVER_PORT);
+
+                while (true) {
+                    Socket clientSocket = serverSocket.accept();
+                    handleClient(clientSocket);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClient(Socket clientSocket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+
+            String request;
+            while ((request = in.readLine()) != null) {
+                String[] parts = request.split(" ");
+                if (parts.length == 3) {
+                    try {
+                        double operand1 = Double.parseDouble(parts[1]);
+                        double operand2 = Double.parseDouble(parts[2]);
+
+                        if (parts[0].equalsIgnoreCase("ADD")) {
+                            double result = operand1 + operand2;
+                            out.println("Result: " + result);
+                        } else if (parts[0].equalsIgnoreCase("SUB")){
+                            double result = operand1 - operand2;
+                            out.println("Result: " + result);
+                        } else if (parts[0].equalsIgnoreCase("MUL")) {
+                            double result = operand1 * operand2;
+                            out.println("Result: " + result);
+                        }
+                         else {
+                            out.println("Unknown operation");
+                        }
+                    } catch (NumberFormatException e) {
+                        out.println("Invalid operands");
+                    }
+                } else {
+                    out.println("Invalid request format");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
