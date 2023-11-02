d3fc579db890e3c3606fde809aa3402e4de6dc77
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2023-11-02 11:32:31 +0100

    Correction bug

--- code/vbonzon_GuillaumeDnt2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/vbonzon_GuillaumeDnt2/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -10,8 +10,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Client {
-    final String SERVER_ADDRESS = "localhost";
-    final int SERVER_PORT = 3141;
+    String SERVER_ADDRESS = "192.168.82.201";
+    final int SERVER_PORT = 31415;
     final String[] OPERATIONS = {"ADD", "SUB"};
     final Charset ENCODING = StandardCharsets.UTF_8;
 
@@ -28,6 +28,7 @@ public class Client {
     }
 
     private void run() {
+
         //Connect to the server
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), ENCODING));
@@ -37,19 +38,20 @@ public class Client {
             System.out.println(in.readLine());
             
             //Start the looping for command to send
-            String userInput = "";
             Scanner scn = new Scanner(System.in);
-            while(true){
+            while(!socket.isClosed()){
                 //Get the command from the user and sends it
                 System.out.print("Enter your command: ");
-                userInput = scn.nextLine();
-                out.write(userInput + "\n");
+                out.write(scn.nextLine() + "\n");
                 out.flush();
 
+                //Wait for the server result
+                String result = in.readLine();
+
                 //End the connection if the command is CLOSE
-                if(userInput == "CLOSE"){
-                    in.close();
-                    out.close();
+                if(result.equals("CLOSING")){
+                    System.out.println("Disconnecting ...");
+                    //socket.close();
                     break;
                 }
 
@@ -57,7 +59,7 @@ public class Client {
                 System.out.println(in.readLine());
             }
         } catch (IOException e){
-
+            System.out.println("An exception happened!");
         }
     }
 }
