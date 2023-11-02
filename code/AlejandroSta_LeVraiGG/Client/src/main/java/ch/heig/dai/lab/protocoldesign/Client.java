82934c7f2b46a98dbacbdbd8cc7bdccd22259d89
LeVraiGG <tux.caroline@gmail.com>
2023-11-02 10:40:57 +0100

    client error resolved

--- code/AlejandroSta_LeVraiGG/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/AlejandroSta_LeVraiGG/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,5 +1,8 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
     final String SERVER_ADDRESS = "localhost";
     final int SERVER_PORT = 4242;
@@ -12,16 +15,16 @@ public class Client {
 
     private void run() {
         //Copy of given example on side:
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT)) ;
-        InputStream in = new BufferedInputStream(socket.getInputStream());
-        OutputStream out = new BufferedOutputStream(socket.getOutputStream());){
+        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+             InputStream in = new BufferedInputStream(socket.getInputStream());
+             OutputStream out = new BufferedOutputStream(socket.getOutputStream());) {
             for (int i = 0; i < 10; i++) {
                 out.write(i);
                 out.flush();
                 System.out.println("Echo: " + in.read());
             }
-        } catch(IOException e){
-            System.out.println("Client: exception : " + e);
+        } catch (IOException ex) {
+            System.out.println("Client: exception : " + ex);
         }
     }
 }
