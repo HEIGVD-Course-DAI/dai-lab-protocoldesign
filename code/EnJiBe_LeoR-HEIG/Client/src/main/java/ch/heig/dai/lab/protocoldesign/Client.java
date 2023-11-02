0ebfb6b7de2b3769636f5d3f6345af26fdc72973
LeoR-HEIG <leo.rinsoz@heig-vd.ch>
2023-11-02 13:59:42 +0100

    labo

--- code/EnJiBe_LeoR-HEIG/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/EnJiBe_LeoR-HEIG/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,15 +1,35 @@
-package ch.heig.dai.lab.protocoldesign;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
-    final int SERVER_PORT = 1234;
+public class Client
+{
+    public static void main(String[] args)
+    {
+        String host = "localhost";
+        int port = 42069;
 
-    public static void main(String[] args) {
-        // Create a new client and run it
-        Client client = new Client();
-        client.run();
-    }
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
 
-    private void run() {
+            System.out.println(in.readLine());
+
+            String userInput;
+
+            while ((userInput = stdIn.readLine()) != null)
+            {
+                out.println(userInput);
+
+                String serverResponse = in.readLine();
+                System.out.println(serverResponse);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
--- code/EnJiBe_LeoR-HEIG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/EnJiBe_LeoR-HEIG/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,14 +1,67 @@
-package ch.heig.dai.lab.protocoldesign;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    final int SERVER_PORT = 1234;
 
     public static void main(String[] args) {
-        // Create a new server and run it
-        Server server = new Server();
-        server.run();
-    }
+        int port = 42069;
 
-    private void run() {
-    } 
+        try (ServerSocket serverSocket = new ServerSocket(port);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())))
+        {
+
+            out.println("AVAILABLE OPERATIONS ADD (ex. ADD 10 20) MUL (ex. MUL 30 4 66)");
+
+            String inputLine;
+
+            while ((inputLine = in.readLine()) != null)
+            {
+                String[] tokens = inputLine.split(" ");
+                String command = tokens[0];
+                int[] operands = new int[tokens.length - 1];
+
+                for (int i = 0; i < operands.length; i++)
+                {
+                    operands[i] = Integer.parseInt(tokens[i + 1]);
+                }
+
+                int result;
+
+                switch (command)
+                {
+                    case "ADD":
+                        result = 0;
+                        for (int operand : operands)
+                        {
+                            result += operand;
+                        }
+                        break;
+                    case "MUL":
+                        result = 1;
+                        for (int operand : operands)
+                        {
+                            result *= operand;
+                        }
+                        break;
+                    case "QUIT":
+                        out.println("CLOSED");
+                        return;
+                    default:
+                        out.println("ERROR: Invalid command");
+                        continue;
+                }
+
+                out.println(result);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
