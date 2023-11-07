da1ec5ebf2679fd28ac2ec5ca677da47cb8c4d18
EwanHeig <ewan.bloechle@heig-vd.ch>
2023-11-07 10:27:41 +0100

    client-server

--- code/EwanHeig/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/EwanHeig/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,8 +1,12 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
-    final int SERVER_PORT = 1234;
+    final String SERVER_ADDRESS = "10.192.94.57";//todo: comment savoir bonne addresse ?
+    final int SERVER_PORT = 4269;
 
     public static void main(String[] args) {
         // Create a new client and run it
@@ -11,5 +15,37 @@ public class Client {
     }
 
     private void run() {
+        boolean quit = false,srvReady = false;
+        String usrIn = "";
+        String SRV_READY = "READY";
+        try {
+            Socket clientSocket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+            BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+
+            //si le serveur est connecté et a fini d'afficher le message de bienvenu
+            if( in.readLine().equals(SRV_READY)){
+                srvReady = true;
+            }
+
+            if (srvReady){
+                while(!quit){
+                    usrIn = stdin.readLine();
+                    if(usrIn == "QUIT"){
+                        quit = true;
+                    }else{
+                        out.write(usrIn + "\n");
+                        out.flush();
+                    }
+                }
+            }
+
+
+
+
+        }catch (IOException e) {
+            System.out.println("Client: exc.: " + e);
+        }
     }
 }
--- code/EwanHeig/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/EwanHeig/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,14 +1,109 @@
 package ch.heig.dai.lab.protocoldesign;
 
-public class Server {
-    final int SERVER_PORT = 1234;
+import java.io.*;
+import java.net.*;
+import static java.nio.charset.StandardCharsets.*;
 
-    public static void main(String[] args) {
-        // Create a new server and run it
+public class Server{
+    final int SERVER_PORT = 4269;
+    private String[] OP = {"ADD","SUB","DIV","MULT"};
+    String SRV_READY = "READY", ERROR = "ERROR:calculation impossible";
+
+    public static void main(String[]args){
+        //Create a new server and run it
         Server server = new Server();
         server.run();
     }
 
-    private void run() {
-    } 
+    private void run(){
+        ServerSocket serverSocket = null;
+        try{
+            serverSocket = new ServerSocket(SERVER_PORT);
+            System.out.println("Serv On");
+
+        }
+        catch(IOException e){
+            System.out.println("Client:exc.:"+e);
+        }
+
+
+        while(true){
+            try{
+                System.out.println("1");
+                Socket clientSocket = serverSocket.accept();//todo: problème ici
+                System.out.println("2");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),UTF_8));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+                String line = in.readLine();
+
+                boolean keepRunning = true;
+
+                out.println("Welcome ! Here are the available operations :");
+                for(String op : OP){
+                    out.println(op);
+                }
+                out.println("enter QUIT to leave");
+                out.println(SRV_READY);
+
+                out.flush();
+
+                while(keepRunning){
+                    //if the command to quit is sent
+                    if(line.equalsIgnoreCase("QUIT")){
+                        keepRunning = false;
+                        out.flush();
+                        continue;
+                    }
+
+                    String[] operation = line.split("");
+                    if(operation.length != 3){
+                        out.println(ERROR);
+                    }
+                    else{
+                        String operator = operation[0];
+                        int val1 = Integer.parseInt(operation[1]);
+                        int val2 = Integer.parseInt(operation[2]);
+                        int result;
+
+                        switch(operator){
+                            case"ADD":
+                                result = val1 + val2;
+                                out.println(result);
+                                break;
+
+                            case"SUB":
+                                result = val1 - val2;
+                                out.println(result);
+                                break;
+                            case"DIV":
+                                if(val2 == 0){
+                                    out.println(ERROR);
+                                }
+                                else{
+                                    result = val1 / val2;
+                                    out.println(result);
+                                }
+                                break;
+                            case"MULT":
+                                result = val1 * val2;
+                                out.println(result);
+                                break;
+                            default:
+                                out.println(ERROR);
+                        }
+                    }
+                    out.flush();
+                }
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+            catch(IOException e){
+                System.out.println("Server:socketex.:"+e);
+            }
+        }
+
+    }
 }
+
