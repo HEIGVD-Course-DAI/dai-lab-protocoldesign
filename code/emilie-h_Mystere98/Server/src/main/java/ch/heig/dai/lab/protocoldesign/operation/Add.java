379d3fa0c85a43a25869463ba367dc95e9616fba
emilie-h <emilie.bressoud@heig-vd.ch>
2023-11-08 17:46:06 +0100

    .java

--- code/emilie-h_Mystere98/Server/src/main/java/ch/heig/dai/lab/protocoldesign/operation/Add.java
+++
@@ -1,7 +0,0 @@
-package ch.heig.dai.lab.protocoldesign.operation;
-
-public class Add {
-    public static double make(int a, int b) {
-        return a + b;
-    }
-}
--- code/emilie-h_Mystere98/Server/src/main/java/ch/heig/dai/lab/protocoldesign/operation/Mul.java
+++
@@ -1,7 +0,0 @@
-package ch.heig.dai.lab.protocoldesign.operation;
-
-public class Mul {
-    public static double make(int a, int b) {
-        return a * b;
-    }
-}
