ace7ffe7e44a4bdbcf047ac3ad76dc2f6e6e100e
emilie-h <emilie.bressoud@heig-vd.ch>
2023-11-08 17:34:15 +0100

    draft Client

--- code/emilie-h_Mystere98/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/emilie-h_Mystere98/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -2,11 +2,12 @@ package ch.heig.dai.lab.protocoldesign;
 
 //import com.google.gson.Gson;
 
+
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import ch.heig.dai.lab.protocoldesign.models.Calculate;
-
+import ch.heig.dai.lab.protocoldesign.*;
+import com.google.gson.Gson;
 
 public class Client {
     final String SERVER_ADDRESS = "localhost";
@@ -16,30 +17,36 @@ public class Client {
         // Create a new client and run it
         Client client = new Client();
         client.run();
-
     }
 
     private void run() {
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT); var in = new BufferedReader( new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
         var out = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))){
+            // Read the Json operation from server
+            String jsonOperation = in.readLine();
+            //print in console the json operations received
+            System.out.println("Server: " + jsonOperation);
+
+            // Ask the server to calculate 1 + 2
+
+
             // Create a JSON request using Gson
-            int[] values = {1, 2};
-            Calculate request = new Calculate(values, "add");
-//            Gson gson = new Gson();
-//            String jsonRequest = gson.toJson(request);
-//
-//            // Send the JSON request to the server
-//            out.println(jsonRequest);
-//
-//            // Receive and parse the JSON response from the server
-//                String jsonResponse = in.readLine();
-            //Calculate response = gson.fromJson(jsonResponse, Calculate.class);
-//            int result = response.getResult();
-            int result = 0;
-            String response = "";
-            System.out.println("Result: " + result);
+
+            Request request = new Request(Type.ADD, 1., 2.);
+            Gson gson = new Gson();
+            String jsonRequest = gson.toJson(request);
+
+            // Send the JSON request to the server
+            out.write(jsonRequest);
+            out.newLine();
+            // Flush the output stream
             out.flush();
-            System.out.println("Client: " + response);
+
+            // Read the JSON response from the server
+            String jsonResponse = in.readLine();
+            Result response = gson.fromJson(jsonResponse, Result.class);
+            // Print the result
+            System.out.println("Result: " + response.getResult());
         }  catch (IOException e){
             System.out.println("Client: exc.: " + e);
         }
