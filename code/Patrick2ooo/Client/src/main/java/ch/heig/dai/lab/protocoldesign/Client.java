9c082541e878c90f3b9f9000deecb8bcfc4eca86
Patrick2ooo <p.maillard1522@gmail.com>
2023-11-09 11:19:42 +0100

    End of Lab

--- code/Patrick2ooo/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
+++ code/Patrick2ooo/Client/src/main/java/ch/heig/dai/lab/protocoldesign/Client.java
@@ -1,15 +1,61 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 public class Client {
-    final String SERVER_ADDRESS = "1.2.3.4";
+    final String SERVER_ADDRESS = "localhost";
     final int SERVER_PORT = 1234;
+    static boolean dontStop = true;
 
     public static void main(String[] args) {
-        // Create a new client and run it
-        Client client = new Client();
-        client.run();
+       // Create a new client and run it
+        while(dontStop){
+            Client client = new Client();
+            client.run();
+        }
     }
 
     private void run() {
+        System.out.println("Client running");
+        try (
+            Socket clientSocket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            var in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(),StandardCharsets.UTF_8));            
+            var out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(),StandardCharsets.UTF_8))){ 
+            out.write("INIT" + "\n");
+            out.flush();
+            String[] init = in.readLine().split(" ");
+            System.out.println("Hello, here's the possible operation: ");
+            for(String i : init){
+                System.out.println(i);
+            }            
+            while(dontStop){
+                System.out.println("Enter your calculation : ");
+                BufferedReader  userInput = new BufferedReader(new InputStreamReader(System.in));
+                String userInputString = userInput.readLine();
+                out.write(userInputString + "\n");
+                out.flush(); 
+                
+                
+                String result = in.readLine();
+                
+                System.out.println("Server response: " + result);
+                if(result.equals("Program exited")){
+                    dontStop = false;
+                    clientSocket.close();
+                    break;                  
+                }
+                
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Client: exception : " + e);
+        }
     }
 }
--- code/Patrick2ooo/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/Patrick2ooo/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,14 +1,78 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class Server {
     final int SERVER_PORT = 1234;
 
+    private static String calculation(String input) {
+        String[] i = input.split(" ");       
+        switch (i[0]) {
+            case "INIT":
+                return "-ADD -SUB -MUL -DIV";
+
+            case "ADD":
+                return (Integer.toString(Integer.parseInt(i[1]) + Integer.parseInt(i[2])));
+                
+            case "SUB":
+                return (Integer.toString(Integer.parseInt(i[1]) - Integer.parseInt(i[2]))) ;
+            
+            case "MUL":
+                return (Integer.toString(Integer.parseInt(i[1]) * Integer.parseInt(i[2]))) ;
+            
+            case "DIV":
+                return (Integer.toString(Integer.parseInt(i[1]) / Integer.parseInt(i[2]))) ;    
+        
+            default:
+                return "Unknown operation";
+        }
+    } 
     public static void main(String[] args) {
         // Create a new server and run it
-        Server server = new Server();
-        server.run();
+        System.out.println("Server running");
+        while(true){
+            Server server = new Server();
+            server.run();
+        }
     }
 
     private void run() {
+        try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
+        
+        while (true) {
+            try (Socket socket = serverSocket.accept();
+            var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));                
+            var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))){
+                String i;               
+                while((i = in.readLine()) != null){    
+                    if(i.equals("QUIT")){
+                        out.write("Program exited \n");
+                        out.flush();
+                        break;
+                    }              
+                    
+                    System.out.println(i);
+                    out.write(calculation(i) + "\n");
+                    out.flush();
+                    
+                }
+                } catch (IOException e) {
+                System.out.println("Server: socket ex.: " + e);
+                }
+            } // while(true)
+        } catch (IOException e) {
+            System.out.println("Server: server socket ex.: " + e);
+        } 
     } 
+
+
 }
